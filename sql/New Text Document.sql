31377fa864ccaafbcfebc7b4ccaea8fc241d59ef
jarchgit <jarchavlis@gmail.com>
2026-04-28 23:46:50 -0400

    Delete New Text Document.sql

--- sql/New Text Document.sql
+++
@@ -1,8 +0,0 @@
--- test_violations.sql
-CREATE TABLE myTable (
-  id NUMBER,
-  customerName VARCHAR2(100),
-  CONSTRAINT pk_test PRIMARY KEY (id)
-);
-
-CREATE INDEX idx_test ON myTable (customerName);
