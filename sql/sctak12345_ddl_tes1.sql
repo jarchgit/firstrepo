0419b94cbd76dd82102215fae7463f585bc6be89
jarchgit <jarchavlis@gmail.com>
2026-04-30 22:41:09 -0400

    sd

--- sql/sctak12345_ddl_tes1.sql
+++ sql/sctak12345_ddl_tes1.sql
@@ -1,14 +1,14 @@
-CREATE TABLE redef_tab (
+CREATE TABLE demo.redef_tab (
   id           NUMBER,
   description  VARCHAR2(50),
   CONSTRAINT redef_tab_pk PRIMARY KEY (id)
 );
 
-CREATE INDEX redef_tab_desc_i ON redef_tab(description);
+CREATE INDEX demo.redef_tab_desc_i ON redef_tab(description);
 
-CREATE SEQUENCE redef_tab_seq;
+CREATE SEQUENCE demo.redef_tab_seq;
 
-CREATE OR REPLACE TRIGGER redef_tab_bir
+CREATE OR REPLACE TRIGGER demo.edef_tab_bir
 BEFORE INSERT ON redef_tab
 FOR EACH ROW
 WHEN (new.id IS NULL)
